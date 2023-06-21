c3514d4963a9124c1f60c91d3e9ed9647ba947fd
jojongguk <jojongguk@factorial.games>
2023-06-21 18:50:27 +0900

    comment lsp line

--- astronvim/user/plugins/user.lua
+++ astronvim/user/plugins/user.lua
@@ -1,25 +1,25 @@
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.motion.nvim-surround" },
-  { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
-  { import = "astrocommunity.bars-and-lines.heirline-vscode-winbar" },
-  { import = "astrocommunity.editing-support/todo-comments-nvim" },
-  -- { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
-  -- { import = "astrocommunity.diagnostics.trouble-nvim" },
-  require("telescope").setup({
-    defaults = {
-      vimgrep_arguments = {
-        "rg",
-        "--color=never",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        "--smart-case",
-      },
-    },
-  }),
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.completion.copilot-lua-cmp" },
+	{ import = "astrocommunity.pack.rust" },
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.motion.nvim-surround" },
+	-- { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
+	{ import = "astrocommunity.bars-and-lines.heirline-vscode-winbar" },
+	{ import = "astrocommunity.editing-support/todo-comments-nvim" },
+	-- { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
+	-- { import = "astrocommunity.diagnostics.trouble-nvim" },
+	require("telescope").setup({
+		defaults = {
+			vimgrep_arguments = {
+				"rg",
+				"--color=never",
+				"--no-heading",
+				"--with-filename",
+				"--line-number",
+				"--column",
+				"--smart-case",
+			},
+		},
+	}),
 }
