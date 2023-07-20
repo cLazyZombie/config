2da525fed173c6dc17a4cd4a72d3cbb45cc17752
jojongguk <jojongguk@factorial.games>
2023-07-20 13:46:50 +0900

    copilot suggestion next/prev to c-j/c-k

--- astronvim/user/plugins/user.lua
+++ astronvim/user/plugins/user.lua
@@ -1,6 +1,20 @@
 return {
 	"AstroNvim/astrocommunity",
 	{ import = "astrocommunity.completion.copilot-lua-cmp" },
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = { "zbirenbaum/copilot.lua" },
+		opts = function(_, opts)
+			local cmp, copilot = require "cmp", require "copilot.suggestion"
+			opts.mapping["<C-j>"] = cmp.mapping(function()
+				if copilot.is_visible() then copilot.next() end
+			end)
+			opts.mapping["<C-k>"] = cmp.mapping(function()
+				if copilot.is_visible() then copilot.prev() end
+			end)
+			return opts
+		end,
+	},
 	{ import = "astrocommunity.pack.rust" },
 	{ import = "astrocommunity.pack.lua" },
 	{ import = "astrocommunity.motion.nvim-surround" },
