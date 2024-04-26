886661ebc7bf405af1b7f2e3b2d5e951733c4038
jojongguk <rusad96@gmail.com>
2024-04-26 20:02:31 +0900

    highlight on yank

--- nvchad/lua/options.lua
+++ nvchad/lua/options.lua
@@ -5,5 +5,9 @@ require "nvchad.options"
 local o = vim.o
 o.cursorlineopt ='both' -- to enable cursorline!
 
+-- set relativenumber
 vim.wo.number = true
 vim.wo.relativenumber = true
+
+-- highlight on yank
+vim.cmd("au TextYankPost * silent! lua vim.highlight.on_yank()")
