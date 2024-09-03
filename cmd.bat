f7142ff914d384738eb0e2c1b5805d78cd24656c
jojongguk <jojongguk@polestargames.com>
2024-09-03 17:59:34 +0900

    ct -> test all targets and features

--- cmd.bat
+++ cmd.bat
@@ -2,7 +2,7 @@
 
 doskey ls=lsd $*
 doskey ll=lsd -a -l $*
-doskey ct=cargo nextest run --workspace --status-level fail
+doskey ct=cargo nextest run --workspace --all-targets --all-features --status-level fail
 doskey cy=cargo clippy --workspace --all-targets --all-features -- -D warnings
 doskey cr=cargo run
 
