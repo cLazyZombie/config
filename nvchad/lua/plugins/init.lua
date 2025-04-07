9bc1cd37c1c26d8ad40486f89de6c4d06eb06a0f
jojongguk <rusad96@gmail.com>
2025-04-07 20:11:48 +0900

    rustceanvim to 6

--- nvchad/lua/plugins/init.lua
+++ nvchad/lua/plugins/init.lua
@@ -81,7 +81,7 @@ return {
   },
   {
     'mrcjkb/rustaceanvim',
-    version = '^5', -- Recommended
+    version = '^6', -- Recommended
     lazy = false, -- This plugin is already lazy
     config = function(_,_)
       vim.g.rustaceanvim = {
